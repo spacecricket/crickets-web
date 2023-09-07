774ce9911b4df1044941a9dc7e99e96159b11ec5
Jatin Sonavane <jatins@hey.com>
2023-09-07 09:34:20 -0700

    Add photo

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,17 @@ export default function Home() {
         />
       </div>
 
+        <div className="">
+            <Image
+                src="/spacecricket.png"
+                alt="Space Cricket"
+                height={100}
+                width={100}
+                priority
+            />
+        </div>
         <div className="mb-32 grid lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+
             <a
                 href="https://github.com/spacecricket"
                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
